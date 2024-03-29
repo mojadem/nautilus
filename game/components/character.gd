d41b20c576442516a6ec7e17313e343fb216862b
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-03-28 20:07:35 -0400

    character looping and initial implementation of character base scene

---
+++ game/components/character.gd
@@ -0,0 +1,10 @@
+@tool
+extends Node3D
+
+@export var mesh: MeshInstance3D
+@export_enum("Happy", "Sad", "Angry", "Surprised") var expression: int:
+	set(value):
+		expression = value
+		var offset: float = float(value) / 4.0
+		var material: BaseMaterial3D = mesh.get_surface_override_material(1)
+		material.uv1_offset = Vector3(offset, 0, 0)
