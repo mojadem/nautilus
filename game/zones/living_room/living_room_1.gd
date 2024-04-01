ed9ed4884330ef01441509a8454c6a0167303350
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-04-01 18:33:08 -0400

    initial scene animation working

--- game/zones/living_room/living_room_1.gd
+++ game/zones/living_room/living_room_1.gd
@@ -1,15 +1,6 @@
-extends Node3D
+extends XRToolsSceneBase
 
-@onready var animation_player = $AnimationPlayer
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_snap_zone_has_dropped():
