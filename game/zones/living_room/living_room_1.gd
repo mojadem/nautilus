a5ca7423bfb7dceeafc7bc709518d7ef1c88e445
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-04-01 16:41:56 -0400

    testing phone call animation

---
+++ game/zones/living_room/living_room_1.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@onready var animation_player = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_snap_zone_has_dropped():
+	animation_player.play("phone_call")
